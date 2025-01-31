2c99a3c8843b0b0104fa4bec4aad708d439f084d
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-02-01 01:36:47 +0700

    text localization using yolov11

---
+++ src/Text_Localization/text_localization.py
@@ -0,0 +1,27 @@
+import ultralytics
+import json
+from ultralytics import YOLO
+
+yolo_yaml_path = 'yolo_data/data.yaml'
+config_path = 'src/config.json'
+
+def load_json_config(config_path):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        
+    return config
+
+config = load_json_config(config_path)
+
+# Load model
+model = YOLO('yolo11m.pt')
+
+# Train model
+results = model.train(
+    data=yolo_yaml_path,
+    epochs=config['yolov11']['epochs'],
+    imgsz=config['yolov11']['image_size'],
+    cache=config['yolov11']['cache'],
+    patience=config['yolov11']['patience'],
+    plots=config['yolov11']['plots']
+)
